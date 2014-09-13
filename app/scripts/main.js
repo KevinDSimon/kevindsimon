02c71fc6fa4636550292fc0efc065144a3b7ab19
KevinDSimon <KevinDSimon@aol.com>
2014-09-13 14:57:37 -0500

    Initial import of off canvas navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    $('.lines').click(function(){
+        $('#main-nav').toggleClass('nav-active');
+        $('.lines').toggleClass('close');
+    });
+});
