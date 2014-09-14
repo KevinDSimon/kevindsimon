cec53aa681bea5d525972a8076b7d29f5e0e1b0c
KevinDSimon <KevinDSimon@aol.com>
2014-09-13 20:19:01 -0500

    Add images, parallax, floating logo, waypoints and main navigation styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
+    //Toggle off-canvas navigation
     $('.lines').click(function(){
         $('#main-nav').toggleClass('nav-active');
         $('.lines').toggleClass('close');
+    });
+    //Execute Full Height of Intro Div
+    $('#hero') .css({'height': (($(window).height()))+'px'});
+        $(window).resize(function(){
+    });
+    //Parallax
+    $objWindow = $(window);
+        $('div[data-type="background"]').each(function(){
+        if (!$('html').hasClass('touch')) {
+            var $bgObj = $(this);
+            $(window).scroll(function() {
+                var yPos = -($(window).scrollTop() / $bgObj.data('speed'));
+                var coords = '100% '+ yPos + 'px';
+                // Change the position of background
+                $bgObj.css({ backgroundPosition: coords });
+            });     
+        }
+    });
+    //Waypoints
+    $('#about').waypoint(function(direction) {
+        // do not show on windows under 1200
+        if ($(window).width() <= 1200){ 
+            $('.logo').hide();
+        }
+        // do this on the way down
+        else if (direction === 'down') {
+            $('.logo').fadeIn('slow');
+        }
+        // do this on the way back up through the waypoint
+        else {
+            $('.logo').fadeOut('slow');
+        }       
     });
 });
