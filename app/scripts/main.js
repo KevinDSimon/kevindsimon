a384582dcbf0c09a2fb973c0f14a17bc78c834cd
KevinDSimon <KevinDSimon@aol.com>
2014-09-20 17:11:03 -0500

    overlay navigation, leveraging viewport height/width for hero image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,41 +1,7 @@
 $(document).ready(function() {
-    //Toggle off-canvas navigation
+    //Toggle hamburger icon
     $('.lines').click(function(){
-        $('#main-nav').toggleClass('nav-active');
         $('.lines').toggleClass('close');
-    });
-    //Execute Full Height of Intro Div
-    $('#hero') .css({'height': (($(window).height()))+'px'});
-        $(window).resize(function(){
-    });
-    //Parallax
-    $objWindow = $(window);
-        $('div[data-type="background"]').each(function(){
-        if (!$('html').hasClass('touch')) {
-            var $bgObj = $(this);
-            $(window).scroll(function() {
-                var yPos = -($(window).scrollTop() / $bgObj.data('speed'));
-                var coords = '100% '+ yPos + 'px';
-                // Change the position of background
-                $bgObj.css({ backgroundPosition: coords });
-            });     
-        }
-    });
-    //Waypoints
-    $('#about').waypoint(function(direction) {
-        // do not show on windows under 1200
-        if ($(window).width() <= 1200){ 
-            $('.logo').hide();
-        }
-        // do this on the way down
-        else if (direction === 'down') {
-            $('.logo').fadeIn('slow');
-            $('.lines').addClass('black');
-        }
-        // do this on the way back up through the waypoint
-        else {
-            $('.logo').fadeOut('slow');
-            $('.lines').removeClass('black');
-        }       
+        $('.overlay').toggleClass('open')
     });
 });
