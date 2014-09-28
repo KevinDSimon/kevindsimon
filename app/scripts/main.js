0359cb6b57154756c9e5bfac97b9b672f874cd09
KevinDSimon <KevinDSimon@aol.com>
2014-09-28 16:46:13 -0500

    Adding parallax, unveil and animation to skills icons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,15 +4,38 @@ $(document).ready(function() {
         $('.lines').toggleClass('close');
         $('.overlay').toggleClass('open')
     });
+    //Parallax
+    $objWindow = $(window);
+        $('header[data-type="background"]').each(function(){
+        if (!$('html').hasClass('touch')) {
+            var $bgObj = $(this);
+            $(window).scroll(function() {
+                var yPos = -($(window).scrollTop() / $bgObj.data('speed'));
+                var coords = '100% '+ yPos + 'px';
+                // Change the position of background
+                $bgObj.css({ backgroundPosition: coords });
+            });     
+        }
+    });
     //Waypoints
     $('#about').waypoint(function(direction) {
+        // hide logo 1200 and under
+        if ($(window).width() <= 1220){ 
+            $('.logo').hide();
+        }
         // do this on the way down
-        if (direction === 'down') {
+        else if (direction === 'down') {
             $('.lines').addClass('black');
+            $('.logo').fadeIn('slow');
         }
         // do this on the way back up through the waypoint
         else {
             $('.lines').removeClass('black');
+            $('.logo').fadeOut('slow');
         }       
+    });
+    // Unveil Images
+    $("#skills img").unveil(-100,function() {
+        $(this).addClass('loaded');
     });
 });
