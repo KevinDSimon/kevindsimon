bd3b01a2411a73b6ecbd3179d4142a3f6ad92ec0
KevinDSimon <kevin.simon@vividseats.com>
2015-02-08 11:07:16 -0600

    Add .pdf images to imagemin grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,7 +246,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= config.app %>/images',
-          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          src: '{,*/}*.{gif,jpeg,jpg,png,pdf}',
           dest: '<%= config.dist %>/images'
         }]
       }
