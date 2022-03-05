1e9767f047161ce751bc9c9780fc2c62c3a8d5de
Kevin Simon <kevinsimon@Kevins-MacBook-Air.local>
2022-03-05 18:43:48 -0500

    Deploy: Regenerate Site

--- docs/_next/static/uPLxa3PcQVtKo5Xve8L6T/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1b631c4d2fd25e6ab7a2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7df6bd4d408439f9b377.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
