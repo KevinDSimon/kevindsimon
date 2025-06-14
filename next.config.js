e4452b2076f616ab6ffe726c299d7269db3378bb
KevinDSimon <kevindsimon@gmail.com>
2025-06-14 13:42:36 -0400

    content updates

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'docs', // Export static files to the 'docs' directory
+};
+
+module.exports = nextConfig;
